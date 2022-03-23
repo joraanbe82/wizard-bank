671ef97557016ad6b39295fc377ffe6371866edd
joraanbe82 <joraanbe@gmail.com>
2022-03-23 16:26:06 +0100

    fix stepper styles in mobile to dont go out of screen

--- src/components/Stepper/StyledStepper.ts
+++ src/components/Stepper/StyledStepper.ts
@@ -4,11 +4,24 @@ import Step from '@mui/material/Step'
 import { Colors } from '../../styles/Colors'
 
 export const StyledStep = styled(Step)`
+	// active step
 	.css-1u4zpwo-MuiSvgIcon-root-MuiStepIcon-root.Mui-active {
 		color: ${Colors.oficialBlue};
 	}
 
+	// complete step
 	.css-1u4zpwo-MuiSvgIcon-root-MuiStepIcon-root.Mui-completed {
 		color: ${Colors.oficialRed};
+	}
+
+	// step label firs mobile
+	.css-ascpo7-MuiStepLabel-root {
+		flex-direction: column;
+	}
+
+	@media (min-width: 640px) {
+		.css-ascpo7-MuiStepLabel-root {
+			flex-direction: row;
+		}
 	}
 `
